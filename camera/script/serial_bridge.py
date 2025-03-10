bb1d234e22808cea478a5cbcd3298035b980aa1d
taz123456789 <anar.tazdarbayeva@gmail.com>
2025-03-10 10:45:08 +0400

    Update serial_bridge.py

--- camera/script/serial_bridge.py
+++ camera/script/serial_bridge.py
@@ -3,35 +3,27 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 import serial
 
-class SerialBridge(Node):
+class ESP32AngleControl(Node):
     def __init__(self):
-        super().__init__('serial_bridge')
-        
-        self.serial_port = serial.Serial('COM8', 115200, timeout=1)
-        
+        super().__init__('esp32_angle_control')
         self.subscription = self.create_subscription(
             Float32,
-            'target_angle',
-            self.serial_callback,
-            10
-        )
+            'target_angle', 
+            self.listener_callback,
+            10)
+        
+        self.serial_port = serial.Serial('COM11', 115200, timeout=1)
 
-    def serial_callback(self, msg):
-        angle = msg.data
-        try:
-            self.serial_port.write(f"{angle}\n".encode('utf-8'))
-            self.serial_port.flush()
-            read = self.serial_port.readline()
-            self.get_logger().info(f"Serial read: {read}")
-            self.get_logger().info(f"Sent angle: {angle}")
-        except Exception as e:
-            self.get_logger().error(f"Serial write failed: {str(e)}")
+    def listener_callback(self, msg):
+        angle = msg.data  # Target angle 
+        self.serial_port.write((str(angle) + "\n").encode())  # Send to ESP32
+        self.get_logger().info(f"Sent target angle: {angle}")
 
-def main():
-    rclpy.init()
-    node = SerialBridge()
-    rclpy.spin(node)
-    node.destroy_node()
+def main(args=None):
+    rclpy.init(args=args)
+    esp32_angle_control = ESP32AngleControl()
+    rclpy.spin(esp32_angle_control)
+    esp32_angle_control.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
