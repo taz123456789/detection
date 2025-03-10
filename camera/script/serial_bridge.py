00ff3fcf412a11dace1751332263bccd8612c8ed
taz123456789 <anar.tazdarbayeva@gmail.com>
2025-03-10 11:26:49 +0400

    Update serial_bridge.py

--- camera/script/serial_bridge.py
+++ camera/script/serial_bridge.py
@@ -11,13 +11,33 @@ class ESP32AngleControl(Node):
             'target_angle', 
             self.listener_callback,
             10)
+        self.subscription = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.listener_callback,
+            10)
         
         self.serial_port = serial.Serial('COM11', 115200, timeout=1)
 
+    # def listener_callback(self, msg):
+    #     angle = msg.data  # Target angle 
+    #     self.serial_port.write((str(angle) + "\n").encode())  # Send to ESP32
+    #     self.get_logger().info(f"Sent target angle: {angle}")
     def listener_callback(self, msg):
-        angle = msg.data  # Target angle 
-        self.serial_port.write((str(angle) + "\n").encode())  # Send to ESP32
-        self.get_logger().info(f"Sent target angle: {angle}")
+        # Interpret Twist message (linear.x for forward/backward, angular.z for left/right)
+        command = "STOP"
+        if msg.linear.x > 0:
+            command = "FORWARD"
+        elif msg.linear.x < 0:
+            command = "BACKWARD"
+        elif msg.angular.z > 0:
+            command = "LEFT"
+        elif msg.angular.z < 0:
+            command = "RIGHT"
+        
+        # Send command over Serial
+        self.serial_port.write((command + "\n").encode())
+        self.get_logger().info(f"Sent command: {command}")
 
 def main(args=None):
     rclpy.init(args=args)
